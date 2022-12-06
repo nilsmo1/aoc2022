92cbb615dff4e4c43964dfe7b62273b6fdc3cafc
Nils <nilsmo1@hotmail.com>
2022-12-07 00:56:12 +0100

    refactor

--- day06/main.py
+++ day06/main.py
@@ -2,36 +2,33 @@
 from typing import List, Union
 
 # Q1 & Q2
-def report_first_marker(signal: str, packet: bool) -> int:
+def report_first_marker(signal: str, size: int) -> int:
     for s, _ in enumerate(signal):
-        if     packet and len(set(signal[s:s+4]))  == 4 : return s+4
-        if not packet and len(set(signal[s:s+14])) == 14: return s+14
-    assert False
+        if len(set(signal[s:s+size])) == size: return s+size
+    assert False, f"unreachable, signal: {signal}, size: {size}"
 
 # Input
 def parse_input(file: str) -> Union[List[str], str]:
     with open(file, 'r') as inp:
         output = inp.read().strip().splitlines()
-    if len(output) == 1: return output.pop()
-    return [line.strip() for line in output]
+    return output.pop() if len(output) == 1 else [line.strip() for line in output]
 
 if __name__ == '__main__':
     # Samples
     sample_input = parse_input('sample')
 
     # Tests
-    sample_asserts_p1 = [7 , 5 , 6 , 10, 11]
-    sample_asserts_p2 = [19, 23, 23, 29, 26]
-    for s, sa_p1, sa_p2 in zip(sample_input, sample_asserts_p1, sample_asserts_p2):
-        assert report_first_marker(s, True)  == sa_p1
-        assert report_first_marker(s, False) == sa_p2
+    sample_asserts = [(7,19), (5,23), (6,23), (10,29), (11,26)]
+    for s, (sa_p1, sa_p2) in zip(sample_input, sample_asserts):
+        assert report_first_marker(s,  4) == sa_p1
+        assert report_first_marker(s, 14) == sa_p2
 
     # Puzzle input
     puzzle_input = parse_input('puzzle-input')
 
     # Results
-    q1 = report_first_marker(puzzle_input, True)
-    q2 = report_first_marker(puzzle_input, False)
+    q1 = report_first_marker(puzzle_input,  4)
+    q2 = report_first_marker(puzzle_input, 14)
 
     print(f'Q1: {q1}')
     print(f'Q2: {q2}')
